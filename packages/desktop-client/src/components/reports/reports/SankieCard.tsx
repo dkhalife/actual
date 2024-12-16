fa668283c357859e48c4a05dae8af9abd7cb0170
Dany Khalife <dev@dkhalife.com>
2024-12-15 19:30:22 -0800

    scaffholding for a renameable report card

--- packages/desktop-client/src/components/reports/reports/SankieCard.tsx
+++ packages/desktop-client/src/components/reports/reports/SankieCard.tsx
@@ -1,4 +1,11 @@
 import { SankieWidget } from 'loot-core/types/models';
+import { ReportCard } from '../ReportCard';
+import { View } from '../../common/View';
+import { ReportCardName } from '../ReportCardName';
+import { DateRange } from '../DateRange';
+import { useCallback, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { calculateTimeRange } from '../reportRanges';
 
 type SankieCardProps = {
   widgetId: string;
@@ -15,5 +22,56 @@ export function SankieCard({
   onMetaChange,
   onRemove,
 }: SankieCardProps) {
-  return <div>Hello world</div>;
+  const { t } = useTranslation();
+  const [nameMenuOpen, setNameMenuOpen] = useState(false);
+  const [start, end] = calculateTimeRange(meta?.timeFrame);
+  const [isCardHovered, setIsCardHovered] = useState(false);
+  const onCardHover = useCallback(() => setIsCardHovered(true), []);
+  const onCardHoverEnd = useCallback(() => setIsCardHovered(false), []);
+
+  return (
+    <ReportCard
+      isEditing={isEditing}
+      to={`/reports/sankie/${widgetId}`}
+      menuItems={[
+        {
+          name: 'rename',
+          text: t('Rename'),
+        },
+      ]}
+      onMenuSelect={item => {
+        switch (item) {
+          case 'rename':
+            break;
+
+          default:
+            throw new Error(`Unrecognized selection ${item}`);
+        }
+      }}
+    >
+      <View
+        style={{ flex: 1 }}
+        onPointerEnter={onCardHover}
+        onPointerLeave={onCardHoverEnd}
+      >
+        <View style={{ flexDirection: 'row', padding: 20 }}>
+          <View style={{ flex: 1 }}>
+            <ReportCardName
+              name={meta?.name || t('Sankie')}
+              isEditing={nameMenuOpen}
+              onChange={newName => {
+                onMetaChange({
+                  ...meta,
+                  name: newName,
+                });
+                setNameMenuOpen(false);
+              }}
+              onClose={() => setNameMenuOpen(false)}
+            />
+            <DateRange start={start} end={end} />
+          </View>
+        </View>
+      </View>
+    </ReportCard>
+  );
 }
